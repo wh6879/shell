0612235e60a7829fca8e8577191d08d9bbd50a5e
wh6879 <wh6879@aliyun.com>
2023-04-02 14:17:41 +0800

    auth: wh commit: 修改目录

--- sh/kafka.sh
+++ sh/kafka.sh
@@ -12,14 +12,14 @@ case $1 in
   for host in 192.168.96.129 192.168.96.130 192.168.96.131
   do
     echo "-----------启动 $host kafka--------------"
-    ssh $host "/opt/module/kafka_2.12-3.4.0/bin/kafka-server-start.sh -daemon /opt/module/kafka_2.12-3.4.0/config/server.properties"
+    ssh $host "/opt/module/kafka/bin/kafka-server-start.sh -daemon /opt/module/kafka_2.12-3.4.0/config/server.properties"
   done
 ;;
 "stop")
   for host in 192.168.96.129 192.168.96.130 192.168.96.131
   do
     echo "-----------停止 $host kafka--------------"
-    ssh $host "/opt/module/kafka_2.12-3.4.0/bin/kafka-server-stop.sh"
+    ssh $host "/opt/module/kafka/bin/kafka-server-stop.sh"
   done
 ;;
 esac
