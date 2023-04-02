f683559417016e61ecf0130df8bbb6c2d8b5a870
wh6879 <wh6879@aliyun.com>
2023-04-02 14:20:48 +0800

    auth: wh commit: 修改目录

--- sh/kafka.sh
+++ sh/kafka.sh
@@ -12,7 +12,7 @@ case $1 in
   for host in 192.168.96.129 192.168.96.130 192.168.96.131
   do
     echo "-----------启动 $host kafka--------------"
-    ssh $host "/opt/module/kafka/bin/kafka-server-start.sh -daemon /opt/module/kafka_2.12-3.4.0/config/server.properties"
+    ssh $host "/opt/module/kafka/bin/kafka-server-start.sh -daemon /opt/module/kafka/config/server.properties"
   done
 ;;
 "stop")
